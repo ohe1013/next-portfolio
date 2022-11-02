dcba62698b0b5a7f87b585d03ece6fbf9aad7c12
ohe1013 <dhgusrms23@gmail.com>
2022-11-03 00:14:34 +0900

    notion api 설정

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>오현근 포트폴리오</title>
         <meta name="description" content="오늘도 빡코딩" />
         <link rel="icon" href="/favicon.ico" />
-      </Head>
+      </Head> 
       <section className="flex min-h-screen flex-col items-center text-gray-600 body-font">
           <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <Hero/> 
